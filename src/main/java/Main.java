aeb05134c20f1d0a237d28c9f215fc423e1aac33
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-02-08 11:52:37 +0500

    find

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
@@ -8,5 +10,17 @@ public class Main {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
+
+        //y.xpath("//input[@aria-label='Найти']") - локатор
+        //определяет как и что искать
+        //WebElement inputs = driver.findElements(By.xpath("//input[@aria-label='Найти']"));
+        //WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+        //WebElement input = driver.findElement(By.cssSelector(""));
+        //WebElement input = driver.findElement(By.className(""));
+        //WebElement input = driver.findElement(By.id("")); //сервер генерирует id динамически
+        //WebElement input = driver.findElement(By.linkText(""));//по тексту ссылку (очень долгий метод
+        WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+
+        input.click();
     }
 }
