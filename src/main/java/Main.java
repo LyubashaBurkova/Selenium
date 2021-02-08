1e1075121dee27fb8da93111f58893b9ef3d22e1
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-02-08 15:29:31 +0500

    getAttribute, getText

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,30 +14,17 @@ public class Main {
 
         WebDriver driver = new ChromeDriver();
 
-        //неявное ожидание - задано по умолчанию
-        //manage - настройка
-        //timeouts().implicitlyWait - задаем ожидание
-        //10, TimeUnit.SECONDS - сколько и чего
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); устаревший метод
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //явное ожидание - для определенного элемента мы задаем параметры
-        //создаем экземпляр WebDriverWait - класс, который отвечает за ожидание
-        //передаем ему драйвер и время
-        //вызываем метод until() для установки явного ожидания
-        //в until передаем то, что мы ожидаем
-        //у ExpectedConditions мы вызываем различные методв, кождый метод отвечает за определенное событие
-        //presenceOfElementLocated ожидаем появление элемента
-        //By.id("123435") - этот элемента мы найдем и будем ожидать его появление 10 с
-        WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("123435"))));
-        //в element запишется элемент с id 12345
+        driver.get("https://www.avito.ru");
 
-        driver.get("https://google.com");
+        WebElement element = driver.findElement(By.xpath("(//a[text()='Личные вещи'])[1]"));////a[@target='_self'][text()='Личные вещи'])
 
-        //неявные ожидания работают быстрее
-        WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+        String par = element.getAttribute("pathname");//вытаскиваем значение из properties
 
-        input.click();
+        System.out.println(par);
+
+        WebElement count = driver.findElement(By.cssSelector(".category-with-counters-count-29J0p.category-with-counters-count_item-3tm8b"));
+        System.out.println(count.getText());//строка содержит иникод, но в консоли он преобразуется
     }
 }
