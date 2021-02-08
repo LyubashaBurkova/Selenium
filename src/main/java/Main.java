32c304b47996e486e08a317f39cc734cb6755b3a
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-02-08 16:13:25 +0500

    sendKeys

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,14 +17,26 @@ public class Main {
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.get("https://dev.by/");
+        driver.get("https://udemy.com");
 
-        WebElement element = driver.findElement(By.xpath("//a[text()='Вход']"));
+        WebElement element = driver.findElement(By.xpath("//input[@name='q']"));
+
+        //в наддном случае в выбранный инпут добавится текст и запустится поиск
+        //для каждой буквы будет вызываться 3 события
+        //для большого текста стрит использовать другой метод
+        //лучше использовать ctrl + c, ctrl + v (предварительно добавить в буфер)
+        //в случае с кликом, мы не можем кликнуть, по невидимому объекту, а текст веести можем
+        element.sendKeys("Java", Keys.ENTER);
+
+        //sendKeys можно использовать для загрузки файла,
+        //ввесто текста добавляем полный путь к файлу на компьютере
+
+
 
         //вернет значение свойства
-        String par = element.getCssValue("transition");
+        //String par = element.getCssValue("transition");
 
-        System.out.println(par);
+        //System.out.println(par);
 
       //  WebElement count = driver.findElement(By.cssSelector(".category-with-counters-count-29J0p.category-with-counters-count_item-3tm8b"));
         //System.out.println(count.getText());//строка содержит иникод, но в консоли он преобразуется
