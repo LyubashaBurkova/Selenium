d46fdf38dd7df79dc4bcdd22cbea80ccc06508d0
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-02-11 14:46:23 +0500

    wait

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -17,5 +18,34 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        try {
+            driver.get("https://pagination.js.org/");
+            Thread.sleep(2000);
+
+            List<WebElement> elements = driver.findElements(By.xpath("//div[@class='data-container']/ul/li"));
+            List<WebElement> pages = driver.findElements(By.xpath("//div[@class='paginationjs-pages']/ul/li"));
+
+            String text = elements.get(1).getText();
+            System.out.println(text);
+
+            pages.get(2).click();
+            //ждем когда пропадет первый элемент на листе
+            wait.until(ExpectedConditions.stalenessOf(elements.get(1)));
+
+            //обновили список
+            elements = (List<WebElement>) driver.findElements(By.xpath("//div[@class='data-container']/ul/li"));
+
+            text = elements.get(1).getText();
+            System.out.println(text);
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            Thread.sleep(20000);
+            driver.quit();
+        }
     }
 }
