55fe59fcab90b2250ff8f918720a33aa69af7f88
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-02-16 23:31:04 +0500

    testng_depends

--- src/main/java/smoke/Test1.java
+++ src/main/java/smoke/Test1.java
@@ -28,7 +28,15 @@ public class Test1 {
         System.out.println("Test 1" + db + " " + e2);
     }
 
-    @Test (groups = {"regress"})
+    //передаем метод, от которого будет зависеть данный метод
+    //теперь перед выполнением 11 выполнится 1
+    //по умолчанию жесткая зависимость (если 1 не выполнится, то 11 не запустится)
+    //если передаем alwaysRun = true, то 11 запустится не зависимо от того выполнится 1 или нет
+    //@Test (dependsOnMethods = {"test1"}, alwaysRun = true)
+    //если хотим быть зависимы от группы, то dependsOnGroups
+    //* все тесты этой группы
+    @Test (dependsOnGroups = {"smokeTest.*"}, alwaysRun = true)
+    //@Test (groups = {"regress"})
     public void test11 () {
         System.out.println("Test 11");
     }
