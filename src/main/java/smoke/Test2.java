30becd3f0e1749d84a557c928532652bad843191
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-02-17 00:07:06 +0500

    testng_retry

--- src/main/java/smoke/Test2.java
+++ src/main/java/smoke/Test2.java
@@ -1,10 +1,16 @@
 package smoke;
 
+import org.testng.Assert;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
 public class Test2 {
-    @Test (groups = {"smokeTest", "regress"})
+    //@Test (groups = {"smokeTest", "regress"})
+    @Test (retryAnalyzer = Retry.class)
     public void test2 () {
         System.out.println("Test 2");
+        Assert.fail();
     }
 }
+
