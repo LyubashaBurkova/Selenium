14a96227af14ed6d8edaa4a124034483c5f6cf9f
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-02-16 19:50:36 +0500

    testng_parameter

--- src/main/java/smoke/Test1.java
+++ src/main/java/smoke/Test1.java
@@ -1,14 +1,16 @@
 package smoke;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Test1 {
     //@Test анотация для метода
     //пусть первый тест будет относится к группе тестов (двум группам)
     //это удабно, т.к. можем запускать отдельные группы
+    @Parameters({"db"})
     @Test (groups = {"smokeTest", "regress"})
-    public void test1 () {
-        System.out.println("Test 1");
+    public void test1 (String db) {
+        System.out.println("Test 1" + db);
     }
 
     @Test (groups = {"regress"})
